8f88aa103eabef49b1095285c4c932a5e03a67ea
Caio Cesar <ahcaio@hotmail.com>
2023-03-15 16:01:18 -0300

    App build

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar, SafeAreaView} from 'react-native';
 import Cesta from './src/views/Cesta';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
+      <StatusBar/>
       <Cesta/>
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
